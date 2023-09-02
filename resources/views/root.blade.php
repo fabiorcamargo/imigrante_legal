566faa975b2c75246bf46ff8db94bfc7ca308371
Fabio Camargo <fabiorcamargo@gmail.com>
2023-09-02 18:43:52 -0300

    up

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -9,6 +9,15 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+
+    <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "url": "https://imigrantelegal.com.br",
+          "logo": "https://imigrantelegal.com.br/storage/assets/img/logo.svg"
+        }
+    </script>
     <!-- Meta Pixel Code -->
     <script>
         !function(f,b,e,v,n,t,s)
