206e47476e2c87931a710bb02d4b369403ff4188
Fabio Camargo <fabiorcamargo@gmail.com>
2024-04-09 11:04:02 -0300

    up

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -18,5 +18,13 @@
         {{ $slot }}
     </main>
     
+    <script>
+        (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+            w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+            m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://crm.imigrantelegal.com.br/mtc.js','mt');
+    
+        mt('send', 'pageview');
+    </script>
     <x-footer class="z-0"></x-footer>
 </div>
