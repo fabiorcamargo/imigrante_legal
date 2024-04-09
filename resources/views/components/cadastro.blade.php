07dca152ca3f5111e6d091a7a0b359e17a4a11b7
Fabio Camargo <fabiorcamargo@gmail.com>
2024-04-08 23:31:54 -0300

    up

--- resources/views/components/cadastro.blade.php
+++ resources/views/components/cadastro.blade.php
@@ -1,23 +1,4 @@
-<script type="text/javascript">
-  /** This section is only needed once per page if manually copying **/
-  if (typeof MauticSDKLoaded == 'undefined') {
-      var MauticSDKLoaded = true;
-      var head            = document.getElementsByTagName('head')[0];
-      var script          = document.createElement('script');
-      script.type         = 'text/javascript';
-      script.src          = 'https://mautic.imigrantelegal.com.br/media/js/mautic-form.js?v687b45f7';
-      script.onload       = function() {
-          MauticSDK.onLoad();
-      };
-      head.appendChild(script);
-      var MauticDomain = 'https://mautic.imigrantelegal.com.br';
-      var MauticLang   = {
-          'submittingMessage': "Por favor, aguarde..."
-      }
-  }else if (typeof MauticSDK != 'undefined') {
-      MauticSDK.onLoad();
-  }
-</script>
+<script type="text/javascript" src="//mautic.imigrantelegal.com.br/form/generate.js?id=1"></script>
 
 <div class="hero min-h-screen bg-base-200">
   <div class="hero-content flex-col lg:flex-row-reverse">
@@ -32,76 +13,7 @@
       <div class="card-body">
         <x-authentication-card-logo />
 
-        <style type="text/css" scoped>
-          .mauticform-field-hidden {
-            display: none
-          }
-        </style>
-        <div id="mauticform_wrapper_imirante1" class="mauticform_wrapper">
-          <form autocomplete="false" role="form" method="post"
-            action="https://mautic.imigrantelegal.com.br/form/submit?formId=1" id="mauticform_imirante1"
-            data-mautic-form="imirante1" enctype="multipart/form-data">
-            <div class="mauticform-error" id="mauticform_imirante1_error"></div>
-            <div class="mauticform-message" id="mauticform_imirante1_message"></div>
-            <div class="mauticform-innerform">
-              <div class="mauticform-page-wrapper mauticform-page-1" data-mautic-form-page="1">
-                <div id="mauticform_imirante1_nome1"
-                  class="mauticform-row mauticform-text mauticform-field-1 mauticform-required" data-validate="nome1"
-                  data-validation-type="text">
-                  <label id="mauticform_label_imirante1_nome1" for="mauticform_input_imirante1_nome1"
-                    class="mauticform-label">Nome</label>
-
-                  <input type="text" name="mauticform[nome1]" value="" id="mauticform_input_imirante1_nome1"
-                    class="mauticform-input">
-
-                  <span class="mauticform-errormsg" style="display:none;">Isso é obrigatório.</span>
-                </div>
-                <div id="mauticform_imirante1_sobrenome"
-                  class="mauticform-row mauticform-text mauticform-field-2 mauticform-required"
-                  data-validate="sobrenome" data-validation-type="text">
-                  <label id="mauticform_label_imirante1_sobrenome" for="mauticform_input_imirante1_sobrenome"
-                    class="mauticform-label">Sobrenome</label>
-
-                  <input type="text" name="mauticform[sobrenome]" value="" id="mauticform_input_imirante1_sobrenome"
-                    class="mauticform-input">
-
-                  <span class="mauticform-errormsg" style="display:none;">Isso é obrigatório.</span>
-                </div>
-                <div id="mauticform_imirante1_telefone"
-                  class="mauticform-row mauticform-tel mauticform-field-3 mauticform-required" data-validate="telefone"
-                  data-validation-type="tel">
-                  <label id="mauticform_label_imirante1_telefone" for="mauticform_input_imirante1_telefone"
-                    class="mauticform-label">Telefone</label>
-
-                  <input type="tel" name="mauticform[telefone]" value="" id="mauticform_input_imirante1_telefone"
-                    class="mauticform-input">
-
-                  <span class="mauticform-errormsg" style="display:none;">Isso é obrigatório.</span>
-                </div>
-                <div id="mauticform_imirante1_pais_de_interesse"
-                  class="mauticform-row mauticform-text mauticform-field-4">
-                  <label id="mauticform_label_imirante1_pais_de_interesse"
-                    for="mauticform_input_imirante1_pais_de_interesse" class="mauticform-label">País de
-                    Interesse</label>
-
-                  <input type="text" name="mauticform[pais_de_interesse]" value=""
-                    id="mauticform_input_imirante1_pais_de_interesse" class="mauticform-input">
-
-                  <span class="mauticform-errormsg" style="display:none;"></span>
-                </div>
-
-                <div id="mauticform_imirante1_submit"
-                  class="mauticform-row mauticform-button-wrapper mauticform-field-5">
-                  <button class="btn btn-default mauticform-button" name="mauticform[submit]" value="1"
-                    id="mauticform_input_imirante1_submit" type="submit">Enviar</button>
-                </div>
-              </div>
-            </div><input type="hidden" name="mauticform[formId]" id="mauticform_imirante1_id" value="1">
-            <input type="hidden" name="mauticform[return]" id="mauticform_imirante1_return" value="">
-            <input type="hidden" name="mauticform[formName]" id="mauticform_imirante1_name" value="imirante1">
-
-          </form>
-        </div>
+        <iframe src="//mautic.imigrantelegal.com.br/form/1" width="300" height="300"><p>Your browser does not support iframes.</p></iframe>
 
 
 
