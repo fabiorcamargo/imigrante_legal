9d3f7713646ec21408197b7fdaf04feaeedc8478
Fabio Camargo <fabiorcamargo@gmail.com>
2024-04-11 23:25:53 -0300

    up

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -50,7 +50,7 @@
 <body class="font-sans antialiased">
     @splade
 
-    
+    <script src="//imi.meusestudosead.com.br/focus/1.js" type="text/javascript" charset="utf-8" async="async"></script>
 </body>
 
 </html>
