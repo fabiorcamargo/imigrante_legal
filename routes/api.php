48c4243e14052ce92f91858a6f9725d568daf68e
Fabio Camargo <fabiorcamargo@gmail.com>
2024-04-15 09:55:40 -0300

    up

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::get('/uf', [UfController::class, 'get'])->name('get_uf');
 Route::get('/uf/{state_id}', [UfController::class, 'name'])->name('get_uf_name');
 Route::get('/uf/{state_id}/region', [UfController::class, 'region'])->name('get_uf_region');
@@ -34,27 +35,31 @@ Route::post('/form_send', function (Request $request) {
 
     //dd($data['mautic.form_on_submit'][0]['submission']['results']);
     // Acesse o email
-    
+
     $dados = [];
-    $dados['agent'] = $data['mautic.form_on_submit'][0]['submission']['results']['agent'];
-    $dados['fbid'] = $data['mautic.form_on_submit'][0]['submission']['trackingId'];
-    $dados['email'] = hash('sha256',$data['mautic.form_on_submit'][0]['submission']['results']['email']);
-    $dados['phone'] = $data['mautic.form_on_submit'][0]['submission']['results']['telefone1'];
-    $dados['fbp'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbp'];
-    $dados['fbc'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbc'];
-    $dados['cidade'] = hash('sha256',strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['cidade']))));
-    $dados['estado'] = hash('sha256',strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['estado']))));
-    $dados['ip'] = $data['mautic.form_on_submit'][0]['submission']['ipAddress']['ipAddress'];
-    $dados['url'] = $data['mautic.form_on_submit'][0]['submission']['referer'];
-    $dados['time'] = strtotime($data['mautic.form_on_submit'][0]['submission']['dateSubmitted']);
-    $dados['country'] = hash('sha256','br');
-    $dados['phone'] = hash('sha256', "55" .   preg_replace('/[^A-Za-z0-9]/', '', $dados['phone']));
+    if (isset($data['mautic.form_on_submit'])) {
+        $dados['agent'] = $data['mautic.form_on_submit'][0]['submission']['results']['agent'];
+        $dados['fbid'] = $data['mautic.form_on_submit'][0]['submission']['trackingId'];
+        $dados['email'] = hash('sha256', $data['mautic.form_on_submit'][0]['submission']['results']['email']);
+        $dados['phone'] = $data['mautic.form_on_submit'][0]['submission']['results']['telefone1'];
+        $dados['fbp'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbp'];
+        $dados['fbc'] = $data['mautic.form_on_submit'][0]['submission']['results']['fbc'];
+        $dados['cidade'] = hash('sha256', strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['cidade']))));
+        $dados['estado'] = hash('sha256', strtolower(str_replace(' ', '', iconv('UTF-8', 'ASCII//TRANSLIT', $data['mautic.form_on_submit'][0]['submission']['results']['estado']))));
+        $dados['ip'] = $data['mautic.form_on_submit'][0]['submission']['ipAddress']['ipAddress'];
+        $dados['url'] = $data['mautic.form_on_submit'][0]['submission']['referer'];
+        $dados['time'] = strtotime($data['mautic.form_on_submit'][0]['submission']['dateSubmitted']);
+        $dados['country'] = hash('sha256', 'br');
+        $dados['phone'] = hash('sha256', "55" .   preg_replace('/[^A-Za-z0-9]/', '', $dados['phone']));
 
 
- 
 
-    $fb = new FacebookApi;
-    $response = $fb->lead2($dados);
+
+        $fb = new FacebookApi;
+        $response = $fb->lead2($dados);
+    } else {
+        $response = 'Dados Externos ' . json_encode($data);
+    }
 
 
     return $response;
