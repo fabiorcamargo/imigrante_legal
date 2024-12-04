21ea491c3096bbd33524075d9ef8413d510d5007
Fabio Camargo <fabiorcamargo@gmail.com>
2024-12-04 15:07:26 -0300

    up

--- config/cors.php
+++ config/cors.php
@@ -2,33 +2,23 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://imigrantelegal.com.br',  // Substitua com suas origens confiáveis
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'https://*.imigrantelegal.com.br',  // Permite subdomínios
+    ],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization'],
 
     'exposed_headers' => [],
 
-    'max_age' => 0,
+    'max_age' => 3600,  // 1 hora
 
-    'supports_credentials' => false,
-
+    'supports_credentials' => true,  // Defina como 'true' apenas se for necessário
 ];
